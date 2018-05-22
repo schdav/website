7107a090d781549146ccdaa0237f1e6966a649cb
schdav <schdav@users.noreply.github.com>
2018-05-22 14:33:31 +0200

    Rearrange resources

--- index.php
+++ index.php
@@ -7,17 +7,12 @@
   <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
   <link rel="manifest" href="/site.webmanifest">
   <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#00adb5">
-  <link rel="stylesheet" href="https://brick.a.ssl.fastly.net/Blogger+Sans:300,400,500,700">
-  <link rel="stylesheet" href="https://cdn.iconmonstr.com/1.3.0/css/iconmonstr-iconic-font.min.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-form-validator/2.3.26/theme-default.min.css" />
   <link rel="stylesheet" href="min/default.min.css">
   <meta charset="utf-8">
   <meta name="description" content="private Webseite von David Schneider &middot; Softwareentwickler in agilen Prozessen">
   <meta name="msapplication-TileColor" content="#ffffff">
   <meta name="theme-color" content="#ffffff">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-form-validator/2.3.26/jquery.form-validator.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.1/moment.min.js"></script>
   <title>David Schneider &middot; Softwareentwickler | SCHDAV</title>
 </head>
@@ -35,6 +30,11 @@
     <?php include('php/counter.php'); ?>
   </div>
 
+  <link rel="stylesheet" href="https://brick.a.ssl.fastly.net/Blogger+Sans:300,400,500,700">
+  <link rel="stylesheet" href="https://cdn.iconmonstr.com/1.3.0/css/iconmonstr-iconic-font.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-form-validator/2.3.26/theme-default.min.css" />
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-form-validator/2.3.26/jquery.form-validator.min.js"></script>
   <script src="min/bundle.min.js"></script>
 </body>
 </html>
