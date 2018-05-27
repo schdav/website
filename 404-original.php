b7986dcce798dc135903a76a144dfa334158c5a9
schdav <schdav@users.noreply.github.com>
2018-05-27 15:24:26 +0200

    Add custom 404 error page

---
+++ 404-original.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+  <link rel="manifest" href="/site.webmanifest">
+  <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#00adb5">
+  <link rel="stylesheet" href="/min/default.min.css">
+  <meta charset="utf-8">
+  <meta name="description" content="Fehlerseite 404">
+  <meta name="msapplication-TileColor" content="#ffffff">
+  <meta name="theme-color" content="#ffffff">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>404 | SCHDAV</title>
+</head>
+
+<body class="error-page">
+  <section class="hero is-fullheight is-primary">
+    <div class="hero-body">
+      <div class="container">
+        <div class="content is-size-4 has-text-centered">
+          <h1>Fehler 404</h1>
+          <p>
+            Seite nicht gefunden!
+            Zurück zu <a class="underline" href="https://www.schdav.de">www.schdav.de</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+</body>
+</html>
