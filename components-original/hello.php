4c43804f57f988251b0be9b7156d02ad663a29d5
schdav <schdav@users.noreply.github.com>
2018-09-06 21:44:17 +0200

    Reduce whitespace

--- components-original/hello.php
+++ components-original/hello.php
@@ -1,28 +1,27 @@
-<section id="hello" class="hero is-fullheight">
-  <div class="hero-body">
-    <div class="container">
-      <div class="content is-size-4">
-        <h1>Hallo</h1>
-        <p>
-          Ich heiße David Schneider, bin
-          <script>
-            document.write(" " + moment().diff(moment([1990, 2, 10]), "years") + " ");
-          </script>
-          Jahre alt und arbeite zurzeit bei <a class="underline" href="https://www.paessler.com/">Paessler AG</a> in Nürnberg.<br />
-          Als Softwareentwickler erstelle ich in agilen Prozessen Webseiten und Webanwendungen.
-        </p>
-        <h2 class="is-size-4">Kenntnisse:</h2>
-        <p>
-          Webentwicklung, C#, Python, Datenbanken, TDD, Unix, ITSM, Scrum
-        </p>
-        <h2 class="is-size-4">Interessen:</h2>
-        <p>
-          Agile Entwicklung, Clean Code, App-Entwicklung, Online Marketing, Webdesign, UX
-        </p>
-      </div>
-      <div class="content has-text-centered">
-        <a class="button is-primary is-outlined" href="https://xing.to/schdav">Weitere Informationen (XING-Profil)</a>
-      </div>
+<section id="hello" class="section is-medium">
+  <div class="container">
+    <div class="content is-size-4">
+      <h1>Hallo</h1>
+      <p>
+        Ich heiße David Schneider, bin
+        <script>
+          document.write(" " + moment().diff(moment([1990, 2, 10]), "years") + " ");
+        </script>
+        Jahre alt und arbeite zurzeit bei <a class="underline" href="https://www.paessler.com/">Paessler AG</a> in
+        Nürnberg.<br />
+        Als Softwareentwickler erstelle ich in agilen Prozessen Webseiten und Webanwendungen.
+      </p>
+      <h2 class="is-size-4">Kenntnisse:</h2>
+      <p>
+        Webentwicklung, C#, Python, Datenbanken, TDD, Unix, ITSM, Scrum
+      </p>
+      <h2 class="is-size-4">Interessen:</h2>
+      <p>
+        Agile Entwicklung, Clean Code, App-Entwicklung, Online Marketing, Webdesign, UX
+      </p>
+    </div>
+    <div class="content has-text-centered">
+      <a class="button is-primary is-outlined" href="https://xing.to/schdav">Weitere Informationen (XING-Profil)</a>
     </div>
   </div>
 </section>
--- components-original/references.php
+++ components-original/references.php
@@ -1,71 +1,71 @@
-<section id="references" class="hero is-fullheight">
-  <div class="hero-body">
-    <div class="container">
-      <div class="content is-size-4">
-        <h1>Referenzen</h1>
-        <div class="columns">
-          <div class="column">
-            <div class="card">
-              <header class="card-header">
-                <span class="card-header-icon">
-                  <span class="icon">
-                    <i class="im im-certificate-o"></i>
-                  </span>
+<section id="references" class="section is-medium">
+  <div class="container">
+    <div class="content is-size-4">
+      <h1>Referenzen</h1>
+      <div class="columns">
+        <div class="column">
+          <div class="card">
+            <header class="card-header">
+              <span class="card-header-icon">
+                <span class="icon">
+                  <i class="im im-certificate-o"></i>
                 </span>
-                <p class="card-header-title">
-                  2013
-                </p>
-              </header>
-              <div class="card-content">
-                <div class="content has-text-centered">
-                  Bachelor of Science (B.Sc.)
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="column">
-            <div class="card">
-              <header class="card-header">
-                <span class="card-header-icon">
-                  <span class="icon">
-                    <i class="im im-certificate-o"></i>
-                  </span>
-                </span>
-                <p class="card-header-title">
-                  2013
-                </p>
-              </header>
-              <div class="card-content">
-                <div class="content has-text-centered">
-                  ITIL&reg; V3 Foundation
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="column">
-            <div class="card">
-              <header class="card-header">
-                <span class="card-header-icon">
-                  <span class="icon">
-                    <i class="im im-certificate-o"></i>
-                  </span>
-                </span>
-                <p class="card-header-title">
-                  2016
-                </p>
-              </header>
-              <div class="card-content">
-                <div class="content has-text-centered">
-                  Certified ScrumMaster&reg; (CSM)
-                </div>
+              </span>
+              <p class="card-header-title">
+                2013
+              </p>
+            </header>
+            <div class="card-content">
+              <div class="content has-text-centered">
+                Bachelor of Science (B.Sc.)
               </div>
             </div>
           </div>
         </div>
-        <p>
-          Eine Auswahl von privaten Projekten ist in meinem <a class="underline" href="https://github.com/schdav">GitHub-Profil</a> oder auf <a class="underline" href="https://play.google.com/store/apps/developer?id=SCHDAV%20Development">Google Play</a> zu finden.
-        </p>
+        <div class="column">
+          <div class="card">
+            <header class="card-header">
+              <span class="card-header-icon">
+                <span class="icon">
+                  <i class="im im-certificate-o"></i>
+                </span>
+              </span>
+              <p class="card-header-title">
+                2013
+              </p>
+            </header>
+            <div class="card-content">
+              <div class="content has-text-centered">
+                ITIL&reg; V3 Foundation
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+            <header class="card-header">
+              <span class="card-header-icon">
+                <span class="icon">
+                  <i class="im im-certificate-o"></i>
+                </span>
+              </span>
+              <p class="card-header-title">
+                2016
+              </p>
+            </header>
+            <div class="card-content">
+              <div class="content has-text-centered">
+                Certified ScrumMaster&reg; (CSM)
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
+      <p>
+        Eine Auswahl von privaten Projekten ist in meinem <a class="underline" href="https://github.com/schdav">GitHub-Profil</a>
+        oder auf <a class="underline" href="https://play.google.com/store/apps/developer?id=SCHDAV%20Development">Google
+          Play</a> zu finden.
+      </p>
     </div>
   </div>
 </section>
