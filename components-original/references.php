1c17d464f9e7b6b4bff3db6b007be1f0d31617c4
David Schneider <schdav@users.noreply.github.com>
2018-10-01 16:09:22 +0200

    Add reference

--- components-original/references.php
+++ components-original/references.php
@@ -61,6 +61,29 @@
           </div>
         </div>
       </div>
+      <div class="columns">
+        <div class="column">
+          <div class="card">
+            <header class="card-header">
+              <span class="card-header-icon">
+                <span class="icon">
+                  <i class="im im-certificate-o"></i>
+                </span>
+              </span>
+              <p class="card-header-title">
+                2018
+              </p>
+            </header>
+            <div class="card-content">
+              <div class="content has-text-centered">
+                Google Zukunftswerkstatt
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="column is-hidden-mobile"></div>
+        <div class="column is-hidden-mobile"></div>
+      </div>
       <p>
         Eine Auswahl von privaten Projekten ist in meinem <a class="underline" href="https://github.com/schdav">GitHub-Profil</a>
         oder auf <a class="underline" href="https://play.google.com/store/apps/developer?id=SCHDAV%20Development">Google
