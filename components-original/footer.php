2c7593383df5e3e80b19533351e0219fc47087d0
David Schneider <schdav@users.noreply.github.com>
2019-06-20 12:10:12 +0200

    Update balloons

--- components-original/footer.php
+++ components-original/footer.php
@@ -2,16 +2,16 @@
   <div class="container">
     <div class="columns has-text-centered">
       <div class="column">
-        <a id="mail-address" class="icon hvr-float" href="#" data-balloon="E-Mail" data-balloon-pos="down">
+        <a id="mail-address" class="icon hvr-float" href="#" aria-label="E-Mail" data-balloon-pos="down">
           <i class="im im-paperplane"></i>
         </a>
-        <a class="icon hvr-float" href="https://github.com/schdav" data-balloon="GitHub" data-balloon-pos="down">
+        <a class="icon hvr-float" href="https://github.com/schdav" aria-label="GitHub" data-balloon-pos="down">
           <i class="im im-github"></i>
         </a>
-        <a class="icon hvr-float" href="https://play.google.com/store/apps/developer?id=SCHDAV%20Development" data-balloon="Google Play" data-balloon-pos="down">
+        <a class="icon hvr-float" href="https://play.google.com/store/apps/developer?id=SCHDAV%20Development" aria-label="Google Play" data-balloon-pos="down">
           <i class="im im-google-play"></i>
         </a>
-        <a class="icon hvr-float" href="https://xing.to/schdav" data-balloon="XING" data-balloon-pos="down">
+        <a class="icon hvr-float" href="https://xing.to/schdav" aria-label="XING" data-balloon-pos="down">
           <i class="im im-xing"></i>
         </a>
       </div>
