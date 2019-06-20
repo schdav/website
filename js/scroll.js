b550905ae4309edf37ec2b3b617c11c924316236
David Schneider <schdav@users.noreply.github.com>
2019-06-20 12:13:13 +0200

    Fix padding when scrolling

--- js/scroll.js
+++ js/scroll.js
@@ -1,7 +1,7 @@
 $(function() {
   $('a[href^="#"]').click(function(event) {
     event.preventDefault();
-    var $padding = $('body').css('padding-top');
+    var $padding = $('#hello .container').css('padding-top');
     var $href = this.href.split('#');
     var $top = $('#' + $href[1]).position().top;
     var $distance = $top - parseInt($padding, 10);
