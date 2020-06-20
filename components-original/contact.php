27700fe91ec29c69bcc884413743c1532190e60c
David Schneider <schdav@users.noreply.github.com>
2020-06-20 22:55:02 +0200

    Delete dependencies

--- components-original/contact.php
+++ components-original/contact.php
@@ -4,17 +4,15 @@
     <div class="content is-size-4">
       <form id="contact-form" novalidate>
         <div class="field">
-          <label for="form-name" class="label">Name *</label>
+          <label for="form-name" class="label">Name</label>
           <div class="control">
-            <input id="form-name" name="name" class="input" type="text" data-validation="required"
-              data-validation-error-msg="Name ist ungültig.">
+            <input id="form-name" name="name" class="input" type="text">
           </div>
         </div>
         <div class="field">
-          <label for="form-email" class="label">E-Mail-Adresse *</label>
+          <label for="form-email" class="label">E-Mail-Adresse</label>
           <div class="control">
-            <input id="form-email" name="email" class="input" type="email" data-validation="email"
-              data-validation-error-msg="E-Mail-Adresse ist ungültig.">
+            <input id="form-email" name="email" class="input" type="email">
           </div>
         </div>
         <div class="field">
@@ -30,9 +28,6 @@
           <div class="control">
             <input type="reset" value="Löschen" class="button">
           </div>
-        </div>
-        <div class="is-size-6 has-text-weight-bold">
-          * Pflichtfeld
         </div>
       </form>
     </div>
